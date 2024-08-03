43a9aa84ca3e038d58f65d2790803559b0a8a562
Austin <Austin.reid@keyin.com>
2024-08-03 17:31:28 -0230

    Added UnitTesting for Item and ItemService as well as ItemService and Items Classes

---
+++ src/main/java/com/keyin/Item/ItemsService.java
@@ -0,0 +1,31 @@
+package com.keyin.Item;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class ItemsService {
+
+    @Autowired
+    private ItemRepository itemsRepository;
+
+    public List<Item> getAllItems() {
+        return itemsRepository.findAll();
+    }
+
+    public Optional<Item> getItemById(Long id) {
+        return itemsRepository.findById(id);
+    }
+
+    public Item saveItem(Item item) {
+        return itemsRepository.save(item);
+    }
+
+    public void deleteItem(Long id) {
+        itemsRepository.deleteById(id);
+    }
+}
