e705e6c536b51eb0c85773edef6ce5d81864ff0e
Danielhayward1223 <138681558+Danielhayward1223@users.noreply.github.com>
2024-08-03 22:02:34 -0230

    (in progress villagers)

--- src/main/java/com/keyin/Villager/Villager.java
+++ src/main/java/com/keyin/Villager/Villager.java
@@ -1,10 +1,13 @@
 package com.keyin.Villager;
 
+import java.util.List;
+
+import com.keyin.Item.Item;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
-
 @Entity
 public class Villager {
 
@@ -17,27 +20,66 @@ public class Villager {
 
     private String birthday;
 
+    private List<Item> loves;
+    private List<Item> likes;
+    private List<Item> dislikes;
+
+    public Villager(String name, String birthday, List<Item> loves, List<Item> likes, List<Item> dislikes) {
+        this.name = name;
+        this.birthday = birthday;
+        this.loves = loves;
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+
+    public Villager() {
+    }
+
     public Long getId() {
         return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getBirthday() {
         return birthday;
     }
 
+    public List<Item> getLoves() {
+        return loves;
+    }
+
+    public List<Item> getLikes() {
+        return likes;
+    }
+
+    public List<Item> getDislikes() {
+        return dislikes;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setBirthday(String birthday) {
         this.birthday = birthday;
+    }
+
+    public void setLoves(List<Item> loves) {
+        this.loves = loves;
+    }
+
+    public void setLikes(List<Item> likes) {
+        this.likes = likes;
+    }
+
+    public void setDislikes(List<Item> dislikes) {
+        this.dislikes = dislikes;
     }
 }
