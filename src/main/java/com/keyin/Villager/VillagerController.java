f3217a6ddd61a5d841e6bf8f2a4f0bdfe856c0aa
Danielhayward1223 <daniel.hayward@keyin.com>
2024-08-08 13:49:40 -0230

    Fixed Cors Policy error

--- src/main/java/com/keyin/Villager/VillagerController.java
+++ src/main/java/com/keyin/Villager/VillagerController.java
@@ -1,6 +1,7 @@
 package com.keyin.Villager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/villager")
+@CrossOrigin
 public class VillagerController {
     @Autowired
     private VillagerService villagerService;
